f559eef3e09311fc8a079209c1806c1fad2189b8
arality <arality@gmail.com>
2025-03-28 15:57:03 -0600

    converting to header

--- include/authentication.hpp
+++ include/authentication.hpp
@@ -5,11 +5,15 @@ namespace SpaceTraders::sdk {
 
 class Authentication {
 public:
-  constexpr Authentication(std::string &token) : token(token) {};
+  constexpr Authentication(const std::string &token) : token(token) {};
 
-  operator std::string() const;
+  operator std::string() const { return "Bearer " + token; };
 
-  friend std::ostream &operator<<(std::ostream &os, const Authentication &auth);
+  friend std::ostream &operator<<(std::ostream &os,
+                                            const Authentication &auth) {
+    os << "Bearer " + auth.token;
+    return os;
+  }
 
 private:
   const std::string &token;
--- include/connection.hpp
+++ include/connection.hpp
@@ -11,11 +11,53 @@ typedef std::map<std::string, std::string> Header;
 
 namespace SpaceTraders::sdk {
 
+class Client {
+public:
+  constexpr static Client &getInstance() {
+    static Client instance;
+    return instance;
+  }
+  constexpr ~Client() { RestClient::disable(); }
+
+private:
+  constexpr Client() { RestClient::init(); }
+
+public:
+  Client(Client const &) = delete;
+  void operator=(Client const &) = delete;
+};
+
 class Connection {
 public:
-  constexpr Connection(const Authentication &auth) : auth(auth) {};
-  nlohmann::basic_json<> get(const std::string endpoint,
-                             const Header &header = {}) const;
+  constexpr Connection(const Authentication &auth)
+      : auth(auth), client(Client::getInstance()) {};
+
+
+  /**
+   * @brief Perform a get request to the server
+   * 
+   * @param endpoint the enpoint to make the request to
+   * @param header any additional headers to include
+   * @return constexpr nlohmann::basic_json<> 
+   */
+  constexpr nlohmann::basic_json<> get(const std::string endpoint,
+                                       const Header &header = {}) {
+    auto conn = get_connection(header);
+
+
+
+    const auto response = conn->get(endpoint);
+    switch (response.code) {
+    case 200: {
+      const auto data = nlohmann::json::parse(response.body);
+      return data["data"];
+    }
+    default: {
+      assert(false); // TODO Fix
+    }
+    }
+  };
+
   // nlohmann::basic_json<> put(const std::string endpoint,
   //                            const Header &header = {});
   // nlohmann::basic_json<> post(const std::string endpoint,
@@ -25,8 +67,23 @@ public:
 
 private:
   const Authentication &auth;
-  inline static const std::string base_url = "https://api.spacetraders.io/v2";
-  std::unique_ptr<RestClient::Connection> get_connection(Header header) const;
+  const Client &client;
+  inline static const std::string baseUrl = "https://api.spacetraders.io/v2";
+  constexpr std::unique_ptr<RestClient::Connection>
+  get_connection(Header header) {
+
+    auto conn = std::unique_ptr<RestClient::Connection>(
+        new RestClient::Connection(baseUrl));
+
+    header["Content-Type"] = "application/json";
+    header["Authorization"] = auth;
+    for (const auto &[key, value] : header)
+      conn->AppendHeader(key, value);
+
+    return conn;
+  }
+
+
 
   // Cache cache;
 };
